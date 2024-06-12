fcd362446d68ada00eaf5d1dca4acfdf7170edb9
César Gutarra <cesargme@gmail.com>
2024-06-12 15:54:47 -0500

    aplicación de ocr

--- myclipboard_util.py
+++ myclipboard_util.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pyperclip
 import json
-
+import easyocr
 
 def get_base64_image_from_clipboard():
     try:
@@ -28,6 +28,19 @@ def get_base64_image_from_clipboard():
     except Exception as e:
         return str(e)
 
+def save_image_from_clipboard(file_path):
+    try:
+        # Obtener la imagen del portapapeles
+        image = ImageGrab.grabclipboard()
+
+        # Verificar si hay una imagen en el portapapeles
+        if image is None:
+            raise ValueError("No hay imagen en el portapapeles.")
+
+        # Guardar la imagen en el archivo especificado
+        image.save(file_path, format="JPEG")
+    except Exception as e:
+        print(f"Error al guardar la imagen: {e}")
 
 def show_image_from_base64(base64_image):
     try:
@@ -42,8 +55,16 @@ def show_image_from_base64(base64_image):
     except Exception as e:
         print("Error al mostrar la imagen:", e)
 
-
 def copy_data_to_excel_clipboard(json_str):
+
+    # Ejemplo de uso
+    # json_str = """[
+    #     {"Fecha": "2024-06-01", "Cuánto": 100.50, "Motivo": "Pago servicio", "Tarjeta": "BBVA, soles", "Transferencia": "VERDADERO"},
+    #     {"Fecha": "2024-06-02", "Cuánto": 50.00, "Motivo": "", "Tarjeta": "Pichincha", "Transferencia": "FALSO"},
+    #     {"Fecha": "2024-06-03", "Cuánto": 75.75, "Motivo": "para pagar a junta", "Tarjeta": "BN", "Transferencia": "VERDADERO"}
+    # ]"""
+
+
     # Parsear la cadena JSON a una lista de diccionarios
     try:
         data = json.loads(json_str)
@@ -66,12 +87,22 @@ def copy_data_to_excel_clipboard(json_str):
     print("La tabla ha sido copiada al portapapeles en formato adecuado para Excel.")
     print(array_of_arrays)
 
+# Cambiar la codificación de la consola a UTF-8
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
 
-# Ejemplo de uso
-# json_str = """[
-#     {"Fecha": "2024-06-01", "Cuánto": 100.50, "Motivo": "Pago servicio", "Tarjeta": "BBVA, soles", "Transferencia": "VERDADERO"},
-#     {"Fecha": "2024-06-02", "Cuánto": 50.00, "Motivo": "", "Tarjeta": "Pichincha", "Transferencia": "FALSO"},
-#     {"Fecha": "2024-06-03", "Cuánto": 75.75, "Motivo": "para pagar a junta", "Tarjeta": "BN", "Transferencia": "VERDADERO"}
-# ]"""
+# Crear un objeto lector de OCR
+reader = easyocr.Reader(['es'])
 
-# copy_data_to_excel_clipboard(json_str)
+# Guardar la imagen del portapapeles en temp.jpg
+save_image_from_clipboard("temp.jpg")
+
+# Leer texto de la imagen
+result = reader.readtext("temp.jpg")
+
+# Imprimir el texto extraído
+for detection in result:
+    print(detection[1])
+
+# Cambiar la codificación de la consola de vuelta a cp1252 (opcional)
+sys.stdout.reconfigure(encoding='cp1252')
